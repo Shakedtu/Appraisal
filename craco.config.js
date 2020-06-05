c95df2872bea2767e2f74ac396af04c737474f47
shakedtuvel <shakit@me.com>
2020-06-05 10:43:41 +0300

    add antd

---
+++ craco.config.js
@@ -0,0 +1,9 @@
+const AntdDayjsWebpackPlugin = require('antd-dayjs-webpack-plugin');
+
+module.exports = {
+    webpack: {
+        plugins: [
+            new AntdDayjsWebpackPlugin(),
+        ]
+    }
+}
