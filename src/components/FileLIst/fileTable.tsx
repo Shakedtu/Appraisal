925fb627f66627526edb10e307df2c2296cda4e2
shakedtuvel <shakit@me.com>
2020-06-06 23:07:47 +0300

    git ignore +

--- src/components/FileLIst/fileTable.tsx
+++ src/components/FileLIst/fileTable.tsx
@@ -3,7 +3,6 @@ import { Table } from 'antd';
 import {useTranslation} from "react-i18next";
 
 interface TableData {
-    key?: string;
     id: string;
     insurer: string;
     status: string;
@@ -19,14 +18,22 @@ const FileTable: React.FunctionComponent = () => {
         dataIndex: column,
         key: column
     }))
-    const data: TableData[] = [{
-        key: '1',
-        id: '1',
-        insurer: 'מבוטח כלשהו',
-        status: 'פתוח',
-        openDate: '22/10/2019',
-        clientName: 'מנורה'
-    }] //TODO: fetch real data
+    const data: TableData[] = [
+        {
+            id: '1',
+            insurer: 'מבוטח כלשהו',
+            status: 'פתוח',
+            openDate: '22/10/2019',
+            clientName: 'מנורה'
+        },
+        {
+            id: '2',
+            insurer: "עוד אחד",
+            status: "סגור",
+            openDate: '22/12/2001',
+            clientName: 'מגדל'
+        }
+    ] //TODO: fetch real data
 
     return (
         <Table
