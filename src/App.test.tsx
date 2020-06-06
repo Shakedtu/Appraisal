dec83d5611779b1a9956d5c5de48d7fe64885274
shakedtuvel <shakit@me.com>
2020-06-06 22:15:12 +0300

    Starts file Tables

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders button', () => {
+test('renders title', () => {
   const { getByText } = render(<App />);
-  const ButtonElement = getByText(/Button/i);
-  expect(ButtonElement).toBeInTheDocument();
-});
+
+  const TitleElement = getByText(/Title/i);
+  expect(TitleElement).toBeInTheDocument();
+})
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
-import './App.css';
-import { Button } from 'antd'
+import './App.scss';
+import { Layout, Typography } from 'antd'
 import { useTranslation } from 'react-i18next';
+import FileTable from './components/FileLIst/fileTable';
 
 
 const App = () => {
   const { t } = useTranslation();
+  const { Header, Content } = Layout;
+  const { Title } = Typography;
   return (
     <div className="App">
-      <Button type="primary">{t('button')}</Button>
+      <Header>
+        <Title>{t('header.title')}</Title>
+      </Header>
+      <Content>
+        <FileTable />
+      </Content>
     </div>
   )
 };
