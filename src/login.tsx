8f811b5b384f0a0948fa3b78872da856d1a5799b
DolevEpshtein <adolman@gmail.com>
2020-06-11 20:36:46 +0300

    add auth post route

--- src/login.tsx
+++ src/login.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import * as firebase from 'firebase';
+import axios from 'axios';
 const config = {
   apiKey: "AIzaSyAS91XPmhjwEBUP7Edni69_s4kJ9KlcBEA",
   authDomain: "appraisal-d63ea.firebaseapp.com",
@@ -12,15 +13,27 @@ const config = {
 firebase.initializeApp(config);
 
 var provider = new firebase.auth.OAuthProvider('microsoft.com');
+provider.addScope('profile');
 
 const startLogin = () => {
   firebase.auth().signInWithPopup(provider)
     .then(function (result: firebase.auth.UserCredential) {
       if (result === null) return;
       if (result.credential === null) return;
-      var token = result.credential["accessToken"];
+      const token = result.credential["accessToken"];
       // // The signed-in user info.
-      var user = result.user;
+      const user = result.user;
+      axios.post('/auth', {
+        user: user
+      }, {
+        headers: { Authorization: `Bearer ${ token }` }
+      })
+        .then((response) => {
+          console.log(response);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
     }).catch(function (error) {
       // Handle Errors here.
       var errorCode = error.code;
--- src/server/index.ts
+++ src/server/index.ts
@@ -2,46 +2,25 @@ var express = require('express');
 var logger = require('connect-logger');
 var cookieParser = require('cookie-parser');
 var session = require('cookie-session');
-// const admin = require('firebase-admin');
-var firebase = require('firebase');
+var request = require('request');
+var axios = require('axios');
 
-// const issuerURI = "https://sts.windows.net/4d237583-6e63-43c9-ab9c-4b10fac8a63e/"; //TODO: Provide your Azure AD tenant ID here.
-// const tenantName = "appraisalweb.onmicrosoft.com"; //TODO: Provide your tenant name here. example: companyxyz. (companyxyz.onmicrosoft.com)
-// const clientId = "84432866-eb77-48cf-be4f-86e568cf3d7f"; //TODO: provide your application clientId here.
-// // Minting a Firebase Auth token requires manual wiring of the service account
-// const serviceAccount = require("./serviceAccount.json");
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://appraisal-d63ea.firebaseio.com"
-// });
 var port = 5000;
 var app = express();
-// var provider = new firebase.auth.OAuthProvider('microsoft.com');
-// provider.setCustomParameters({
-//   // Optional "tenant" parameter in case you are using an Azure AD tenant.
-//   // eg. '8eaef023-2b34-4da1-9baa-8bc8c9d6a490' or 'contoso.onmicrosoft.com'
-//   // or "common" for tenant-independent tokens.
-//   // The default value is "common".
-//   tenant: '84432866-eb77-48cf-be4f-86e568cf3d7f'
-// });
-// provider.addScope('mail.read');
-// provider.addScope('calendars.read');
 
-app.get('/', function (req, res) {
-  // firebase.auth().signInWithRedirect(provider);
-  // firebase.auth().getRedirectResult()
-  //   .then(function (result) {
-  //     console.log(result);
-  //     // User is signed in.
-  //     // IdP data available in result.additionalUserInfo.profile.
-  //     // OAuth access token can also be retrieved:
-  //     // result.credential.accessToken
-  //     // OAuth ID token can also be retrieved:
-  //     // result.credential.idToken
-  //   })
-  //   .catch(function (error) {
-  //     // Handle error.
-  //   });
+
+app.post('/auth', function (req, res) {
+  const token = req.header('Authorization').replace('Bearer ', '');
+  axios
+    .post('https://graph.microsoft.com/v1.0/me', {
+      headers: { Authorization: `Bearer ${ token }` }
+    })
+    .then((response) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 });
 
 app.listen(port, () => {
